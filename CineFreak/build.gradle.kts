c12a8fef3e04e21e84bc1a161eca3e71517672af
HaxerAhmed <101901219+HaxerAhmed@users.noreply.github.com>
2025-10-21 09:38:46 -0700

    Create build.gradle.kts

---
+++ CineFreak/build.gradle.kts
@@ -0,0 +1,10 @@
+version = 1
+
+cloudstream {
+    language = "en"
+    description = "Scraper for Cinefreak.net movies and series"
+    authors = listOf("jesan")
+    status = 1
+    tvTypes = listOf("Movie", "TvSeries")
+    iconUrl = ""
+}
