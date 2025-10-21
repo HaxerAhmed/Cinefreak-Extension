59cbc4c519e9004a4b94c078d7c7e1fe6899f4f6
HaxerAhmed <101901219+HaxerAhmed@users.noreply.github.com>
2025-10-21 10:18:49 -0700

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,14 +5,12 @@ rootProject.name = "CloudstreamPlugins"
 
 val disabled = listOf("ExampleProvider")
 
+File(rootDir, ".").listFiles()?.filter { it.isDirectory }?.forEach { dir ->
     if (!disabled.contains(dir.name) && File(dir, "build.gradle.kts").exists()) {
         include(dir.name)
     }
 }
 
-fun File.eachDir(block: (File) -> Unit) {
-    listFiles()?.filter { it.isDirectory }?.forEach { block(it) }
-}
-
-// To only include a single project, comment out the previous lines (except the first one), and include your plugin like so:
+// To only include a single project, comment out the previous lines (except the first one),
+// and include your plugin like so:
 // include("PluginName")
